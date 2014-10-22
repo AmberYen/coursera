01fd8881c5197ccc1986d233c4219bde7554b3af
Stanney <freehourse2sh@gmail.com>
2014-10-22 23:28:54 +0800

    update two hw

--- Intro_Computer/Code/numofinterger.cpp
+++ Intro_Computer/Code/numofinterger.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main (){
 
-	int one=0,five=0,ten=0,num[100],n=0;
+	int one=0,five=0,ten=0,num[100],n=0,i=0;
 
-	cin >> n ;
+	cin >> n;
 
 	for(i =0;i < n;i++)
 		cin >> num[i];
@@ -15,18 +15,22 @@ int main (){
 		{
 			case 1:
 				one++;
+				break;
 			case 5:
 				five++;
+				break;
 			case 10:
 				ten++;
+				break;
 			default:
+				break;
 		}
 
 	}
 
-	count << one << endl;
-	count << five <<endl;
-	count << ten << endl;
+	cout << one << endl;
+	cout << five <<endl;
+	cout << ten << endl;
 
 	return 0;
 }
--- Intro_Computer/Code/sumofbinarynum.cpp
+++ Intro_Computer/Code/sumofbinarynum.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 int main()
 {
-	cin >> n;
+	int n,i=0,count[1000],num[1000];
 
+	cin >> n;
+		
+	for(i = 0; i < n; i++)
+		count[i] = 0;
 	
+	for(i = 0; i < n; i++)
+		cin >> num[i];
+
+	for(i = 0; i< n; i++)
+	{
+		while(num[i] != 0)
+		{
+			 count[i] += num[i] % 2;
+			 num[i] = num[i]/2;
+		}	
+
+	}
+
+	for(i = 0; i < n; i++)
+		cout << count[i] << endl;
+
+	return 0;
 
 }
