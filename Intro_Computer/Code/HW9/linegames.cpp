62556a5926b019b6c2f198dbcbfe13062d856965
Stanney <freehourse2sh@gmail.com>
2014-11-25 21:52:25 +0800

    HW9 2-3 completed

--- Intro_Computer/Code/HW9/linegames.cpp
+++ Intro_Computer/Code/HW9/linegames.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 void find_right(char *line,int Index);
+char chr[2];
 int main(){
 
 	char line[100];
 
 	cin >> line;
+	chr[0] = line[0];
 
 	find_right(line,0);
-	system("pause");
 	return 0;
 }
 
@@ -16,14 +17,15 @@ void find_right(char *line,int Index){
 
 	for(int i = Index+1; line[i] != '\0'; i++){
 
-		if(line[i] == '('){
+		if(line[i] == chr[0]){
 			find_right(line,i);
 		}else if(line[i] == 1)
 			continue;
 		else{
 			cout << Index << " " << i << endl;
 			line[i] = 1;
-			line[Index] =1;	
+			line[Index] =1;
+			break;
 		}
 			
 	}
