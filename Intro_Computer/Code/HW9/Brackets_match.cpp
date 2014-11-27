39c0bc679595a61ccaa424f9ca6262d2e8ad05a5
Stanney <freehourse2sh@gmail.com>
2014-11-27 21:27:27 +0800

    HW9 finished

--- Intro_Computer/Code/HW9/Brackets_match.cpp
+++ Intro_Computer/Code/HW9/Brackets_match.cpp
@@ -1,50 +1,58 @@
 #include <iostream>
 using namespace std;
-void find_brackets(char *str,int Index, char *result);
+void find_brackets(char *str,int Index, char *result,int length);
 int main(){
 
-	char str[100], char result[100]={'\0'};
-	cin.getline(str,100);
+	char str[101],str_copy[101], result[101]={'\0'};
+	int length;
+	while(cin.getline(str,101)){
+	
+		for(length = 0; str[length]!='\0';length++){
+			str_copy[length] = str[length];
+		}
 
-	find_brackets(str,0,result);
+		find_brackets(str,0,result,length);
+	
 
-	for(int k = 0; str[k] != '\0'; k++){
-		cout << str[k];
+		for(int k = 0; k < length; k++){
+			cout << str_copy[k];
+		}
+		cout << endl;
+		for(int k = 0; result[k] != '\0'; k++){
+			cout << result[k];
+		}
+
+		cout << endl;
 	}
-	cout << endl;
-	for(int k = 0; result[k] != '\0'; k++){
-		cout << str[k];
-	}
-
-	cout << endl;
 
 	return 0;
 }
 
-void find_brackets(char *str, int Index, char *result){
+void find_brackets(char *str, int Index, char *result,int length){
 
- //for(int j = 0; ) 
-
+if(Index < length){
   if(str[Index] == '('){
   	result[Index] = '$';
   	for(int i= Index+1; str[i] != '\0'; i++){
   		if(str[i] == '('){
-  			find_brackets(str,i,result);
+  			find_brackets(str,i,result,length);
   		}else if(str[i] == ')'){
+			str[Index] = 1;
+			str[i] = 1;
   			result[Index] = ' ';
   			result[i] = ' ';
+			find_brackets(str,Index+1,result,length);
   			break;
   		}
   	}
   }	  
-  else if(str[i] == ')')
+  else if(str[Index] == ')'){
   	  result[Index] = '?';
-
-  /*for(int i = Index; str[i] != '\0'; i++){
-	 if(str[i] == '('){
-	 	find_brackets(str,Index+1,result);
-	 }else if(str[i] == ')'){
-	 	str[Index] = ''
-	 }
-  }*/
+	  find_brackets(str,Index+1,result,length);
+  }
+  else{
+	  result[Index] = ' ';
+	  find_brackets(str,Index+1,result,length);
+  }
+}
 }
